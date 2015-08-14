75a73db1e0953e77feabc0de4187a63df8ac4d94
bbdev001 <alexbest@fromru.com>
2015-08-14 13:28:04 +0300

    Added missed file

--- libproject/google-play-services_lib/src/android/UnusedStub.java
+++ libproject/google-play-services_lib/src/android/UnusedStub.java
@@ -16,6 +16,9 @@
 package android;
 
 // Stub java file to make inclusion into some IDE's work.
-public final class UnusedStub {
-    private UnusedStub() { }
+public final class UnusedStub
+{
+	private UnusedStub()
+	{
+	}
 }
